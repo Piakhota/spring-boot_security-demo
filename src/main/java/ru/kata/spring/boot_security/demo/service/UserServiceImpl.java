eb6b10b42a680bf63d83d7e2b82b73ac9247e788
Mihail Piakhota <piakhota@bk.ru>
2022-11-28 19:57:13 +0400

    fix after review #2: add transactions

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.RoleDao;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
 import ru.kata.spring.boot_security.demo.model.Role;
@@ -29,22 +30,26 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public List<User> getAll() {
         return userDao.findAll();
     }
 
     @Override
+    @Transactional
     public void save(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userDao.save(user);
     }
 
     @Override
+    @Transactional
     public User get(Long id) {
         return userDao.findById(id).orElse(null);
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
         userDao.deleteById(id);
     }
